53881ef60d4fc13cbd0f363537d0ccbd1110acf6
greypk <greypks@gmail.com>
2018-01-05 18:32:21 +0600

    Fixed AdminDAL Tests

--- headhunter/test/DAL/AdminDALTest.java
+++ headhunter/test/DAL/AdminDALTest.java
@@ -7,81 +7,56 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import org.junit.Test;
 
-/**
- *
- * @author Gerune
- */
 public class AdminDALTest {
-    
+
     public AdminDALTest() {
     }
 
-    /**
-     * Test of selectAll method, of class AdminDAL.
-     */
     @Test
     public void testSelectAll() {
         System.out.println("AdminDal selectAll");
         AdminDAL instance = new AdminDAL();
         List<Admin> result = instance.selectAll();
-        assertTrue(result!=null && result.size()>0);
+        assertTrue(result != null && result.size() > 0);
     }
 
-    /**
-     * Test of selectById method, of class AdminDAL.
-     */
     @Test
     public void testSelectById() {
         System.out.println("AdminDal selectById");
         int id = 1;
         AdminDAL instance = new AdminDAL();
         Admin result = instance.selectById(id);
-        assertTrue(result!=null);
+        assertTrue(result != null);
     }
 
-    /**
-     * Test of insert method, of class AdminDAL.
-     */
     @Test
     public void testInsert() {
         System.out.println("insert");
-        Admin admin = null;
+        Admin admin = new Admin(100, "user", "password");
         AdminDAL instance = new AdminDAL();
-        int expResult = 0;
+        int expResult = 1;
         int result = instance.insert(admin);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
-    /**
-     * Test of update method, of class AdminDAL.
-     */
     @Test
     public void testUpdate() {
         System.out.println("update");
-        Admin admin = null;
+        Admin admin = new Admin(100, "newuser", "newpassword");
         AdminDAL instance = new AdminDAL();
-        int expResult = 0;
+        int expResult = 1;
         int result = instance.update(admin);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
-    /**
-     * Test of delete method, of class AdminDAL.
-     */
     @Test
     public void testDelete() {
         System.out.println("delete");
-        Admin admin = null;
+        Admin admin = new Admin(100, null, null);
         AdminDAL instance = new AdminDAL();
-        int expResult = 0;
+        int expResult = 1;
         int result = instance.delete(admin);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
-    
+
 }
