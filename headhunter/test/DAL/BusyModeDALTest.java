a66d0f36e4442575c17b4f4e925dedcd8dffbfb6
Gerune <shisutaki@gmail.com>
2018-01-05 20:09:35 +0600

    Minor MyBatis fixes

--- headhunter/test/DAL/BusyModeDALTest.java
+++ headhunter/test/DAL/BusyModeDALTest.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package DAL;
 
 import java.util.List;
@@ -11,10 +7,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
-/**
- *
- * @author Gerune
- */
+
 public class BusyModeDALTest {
     
     public BusyModeDALTest() {
