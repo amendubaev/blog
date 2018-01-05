9cdc4f8519f1e52eeb52d3deb4cab6b408585a1a
Gerune <shisutaki@gmail.com>
2018-01-05 17:11:50 +0600

    Created Admin controller

--- headhunter/src/java/controllers/AdminController.java
+++ headhunter/src/java/controllers/AdminController.java
@@ -5,10 +5,43 @@
  */
 package controllers;
 
+import DAL.AdminDAL;
+import java.util.List;
+import model.Admin;
+
 /**
  *
  * @author Gerune
  */
 public class AdminController {
+
+    protected AdminDAL adminDal ;
+    public AdminController() {
+        adminDal =  new AdminDAL();
+    }
     
+    public List<Admin> getAllAdmin()
+    {
+        return adminDal.selectAll();
+    }
+    
+    public Admin getAdminById(int id)
+    {
+        return adminDal.selectById(id);
+    }
+    
+    public int insertAdmin(Admin admin)
+    {
+        return adminDal.insert(admin);
+    }
+    public int updateAdmin(Admin admin)
+    {
+        return adminDal.update(admin);
+    }
+    
+     public int delelteAdmin(Admin admin)
+    {
+        return adminDal.delete(admin);
+    }
 }
+
