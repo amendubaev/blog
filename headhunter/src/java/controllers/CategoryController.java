520c5ecbebfed1c49177db3f6bc63378dac8cc6c
Anuar Mendubaev <jocde96@gmail.com>
2018-01-06 17:41:55 +0600

    Added controller for categories

--- headhunter/src/java/controllers/CategoryController.java
+++ headhunter/src/java/controllers/CategoryController.java
@@ -4,11 +4,47 @@
  * and open the template in the editor.
  */
 package controllers;
-
+import DAL.CategoryDAL;
+import java.util.List;
+import model.Category;
+import model.Vacancy;
 /**
  *
  * @author greypk
  */
 public class CategoryController {
     
+    protected CategoryDAL categoryDal ;
+    public CategoryController() {
+        categoryDal =  new CategoryDAL();
+    }
+    
+    public List<Category> selectAll()
+    {
+        return categoryDal.selectAll();
+    }
+
+    public List<Vacancy> selectVacancyForCategory()
+    {
+        return categoryDal.selectVacancyForCategory();
+    }
+    
+    public Category selectById(int id)
+    {
+        return categoryDal.selectById(id);
+    }
+    
+    public int insert(Category category)
+    {
+        return categoryDal.insert(category);
+    }
+    public int update(Category category)
+    {
+        return categoryDal.update(category);
+    }
+    
+     public int delete(Category category)
+    {
+        return categoryDal.delete(category);
+    }
 }
