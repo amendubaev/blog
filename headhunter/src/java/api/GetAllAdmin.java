f2ffd98510ea01250331810e0a589eeb5fa1a31d
greypk <greypks@gmail.com>
2018-01-06 23:51:25 +0600

    Create API /GetAllAdmin

---
+++ headhunter/src/java/api/GetAllAdmin.java
@@ -0,0 +1,45 @@
+package api;
+
+import controllers.AdminController;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mappers.JsonAdmin;
+import model.Admin;
+
+@WebServlet(name = "GetAllAdmin", urlPatterns = {"/GetAllAdmin"})
+public class GetAllAdmin extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            AdminController adminController = new AdminController();
+            List<Admin> admin = adminController.getAllAdmin();
+            String listJson = JsonAdmin.toJSON(admin);
+            out.println(listJson);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
