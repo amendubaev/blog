06a1473c32255aa8b8566fe3a7dab61081965312
Gerune <shisutaki@gmail.com>
2018-01-06 16:01:59 +0600

    Added test for CompanyDAL

---
+++ headhunter/test/DAL/CompanyDALTest.java
@@ -0,0 +1,71 @@
+package DAL;
+
+import java.util.List;
+import model.Company;
+import model.Vacancy;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+
+public class CompanyDALTest {
+    
+    public CompanyDALTest() {
+    }
+
+    @Test
+    public void testSelectVacancyForCompany() {
+        System.out.println("CompanyDal selectVacancyForCompany");
+        CompanyDAL instance = new CompanyDAL();
+        List<Vacancy> result = instance.selectVacancyForCompany();
+        assertTrue(result != null);
+    }
+    
+    @Test
+    public void testSelectAll() {
+        System.out.println("CompanyDal selectAll");
+        CompanyDAL instance = new CompanyDAL();
+        List<Company> result = instance.selectAll();
+        assertTrue(result != null && result.size() > 0);
+    }
+
+    @Test
+    public void testSelectById() {
+        System.out.println("CompanyDal selectById");
+        int id = 1;
+        CompanyDAL instance = new CompanyDAL();
+        Company result = instance.selectById(id);
+        assertTrue(result != null);
+    }
+    
+    @Test
+    public void testInsert() {
+        System.out.println("insert");
+        Company company = new Company(100, "Company", "Description", "98008008080", null);
+        CompanyDAL instance = new CompanyDAL();
+        int expResult = 1;
+        int result = instance.insert(company);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testUpdate() {
+        System.out.println("update");
+        Company company = new Company(100, "Company", "Description", "98008008080", null);
+        CompanyDAL instance = new CompanyDAL();
+        int expResult = 1;
+        int result = instance.update(company);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testDelete() {
+        System.out.println("delete");
+        Company company = new Company(100, "Company", "Description", "98008008080", null);
+        CompanyDAL instance = new CompanyDAL();
+        int expResult = 1;
+        int result = instance.delete(company);
+        assertEquals(expResult, result);
+    }
+   
+}
