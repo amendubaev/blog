03f71082c3da342a8541a30e823447c769b4f38e
Gerune <shisutaki@gmail.com>
2018-01-07 17:00:03 +0600

    Added api GetAllBusyMode and GetAllVacancy

---
+++ headhunter/src/java/api/GetAllVacancy.java
@@ -0,0 +1,45 @@
+package api;
+
+import controllers.VacancyController;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mappers.JsonVacancy;
+import model.Vacancy;
+
+@WebServlet(name = "GetAllVacancy", urlPatterns = {"/GetAllVacancy"})
+public class GetAllVacancy extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            VacancyController vacancyController = new VacancyController();
+            List<Vacancy> vacancy = vacancyController.getAllVacancy();
+            String listJson = JsonVacancy.toJSON(vacancy);
+            out.println(listJson);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
