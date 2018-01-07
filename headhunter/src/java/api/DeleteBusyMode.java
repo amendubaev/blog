7d8fbeddda34b805ca2014c7cade2771908ab11b
Gerune <shisutaki@gmail.com>
2018-01-07 17:19:50 +0600

    Added test for VacancyDAL

--- headhunter/src/java/api/DeleteBusyMode.java
+++ headhunter/src/java/api/DeleteBusyMode.java
@@ -1,14 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package api;
 
-/**
- *
- * @author Gerune
- */
-public class DeleteBusyMode {
+import controllers.BusyModeController;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mappers.JsonBusyMode;
+import model.BusyMode;
+
+@WebServlet(name = "DeleteBusyMode", urlPatterns = {"/DeleteBusyMode"})
+
+public class DeleteBusyMode extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+       String jsonObject = request.getParameter("json");
+        
+        try (PrintWriter out = response.getWriter()) 
+        {
+           BusyMode busymode = JsonBusyMode.fromJSON(jsonObject);
+           BusyModeController  busymodeController = new BusyModeController();
+           int res = busymodeController.deleteBusyMode(busymode);
+           out.print(res);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
     
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
 }
--- headhunter/src/java/api/DeleteCompany.java
+++ headhunter/src/java/api/DeleteCompany.java
@@ -1,14 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package api;
 
-/**
- *
- * @author Gerune
- */
-public class DeleteCompany {
+import controllers.CompanyController;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mappers.JsonCompany;
+import model.Company;
+
+@WebServlet(name = "DeleteCompany", urlPatterns = {"/DeleteCompany"})
+
+public class DeleteCompany extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+       String jsonObject = request.getParameter("json");
+        
+        try (PrintWriter out = response.getWriter()) 
+        {
+           Company company = JsonCompany.fromJSON(jsonObject);
+           CompanyController  companyController = new CompanyController();
+           int res = companyController.deleteCompany(company);
+           out.print(res);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
     
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
 }
