0c2f250ef5903ad86ae1ef834e8ff2f8d968bc53
Gerune <shisutaki@gmail.com>
2018-01-07 13:19:55 +0600

    Created GetAllCategory api

---
+++ headhunter/src/java/api/GetAllCategory.java
@@ -0,0 +1,45 @@
+package api;
+
+import controllers.CategoryController;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mappers.JsonCategory;
+import model.Category;
+
+@WebServlet(name = "GetAllCategory", urlPatterns = {"/GetAllCategory"})
+public class GetAllCategory extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            CategoryController categoryController = new CategoryController();
+            List<Category> category = categoryController.getAllCategory();
+            String listJson = JsonCategory.toJSON(category);
+            out.println(listJson);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
