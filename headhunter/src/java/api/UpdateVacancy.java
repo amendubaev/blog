2f0f8d0a0d5fc882a62bd4703baa04a1ab6d7f96
greypk <greypks@gmail.com>
2018-01-08 20:43:45 +0600

    Add update admin

---
+++ headhunter/src/java/api/UpdateVacancy.java
@@ -0,0 +1,49 @@
+package api;
+
+import controllers.VacancyController;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mappers.JsonVacancy;
+import model.Vacancy;
+
+@WebServlet(name = "UpdateVacancy", urlPatterns = {"/UpdateVacancy"})
+
+public class UpdateVacancy extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        String jsonObject = request.getParameter("json");
+
+        try (PrintWriter out = response.getWriter()) {
+            Vacancy vacancy = JsonVacancy.fromJSON(jsonObject);
+            VacancyController vacancyController = new VacancyController();
+            int res = vacancyController.updateVacancy(vacancy);
+            out.print(res);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
